c1a21e1752f8dc3ab0d3f7afff7ec9fe5f072150
Mike Datsko <mike@datsko.it>
2018-02-16 03:50:59 +0200

    changed loaded url

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,511 @@
-"use strict";function _classCallCheck(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}var _createClass=function(){function e(e,t){for(var a=0;a<t.length;a++){var n=t[a];n.enumerable=n.enumerable||!1,n.configurable=!0,"value"in n&&(n.writable=!0),Object.defineProperty(e,n.key,n)}}return function(t,a,n){return a&&e(t.prototype,a),n&&e(t,n),t}}(),Data=function(){function e(){_classCallCheck(this,e)}return _createClass(e,[{key:"create",value:function(e,t){return e=e||prompt("Key:"),t=t||prompt("Data:"),void 0===e?(console.error("No key"),!1):void 0===t?(console.error("No data"),!1):void localStorage.setItem(e,t)}},{key:"read",value:function(e){if(void 0===e)return console.error("No data"),!1;var t=localStorage.getItem(e);return t||(console.error("No data"),!1)}},{key:"update",value:function(e,t){return e=e||prompt("Key:"),t=t||prompt("Data:"),void 0===e?(console.error("No key"),!1):void 0===t?(console.error("No data"),!1):localStorage.getItem(e)?void localStorage.setItem(e,t):(console.error("No data found"),!1)}},{key:"delete",value:function(e){return void 0===(e=e||prompt("Key:"))?(console.error("No key"),!1):localStorage.getItem(e)?void localStorage.removeItem(e):(console.error("No data found"),!1)}}]),e}(),data=new Data,Events=function(){function e(){_classCallCheck(this,e)}return _createClass(e,[{key:"on",value:function(e,t,a){e.addEventListener(t,a,!1)}},{key:"subscribe",value:function(e,t){document.addEventListener(e,t,!1)}},{key:"send",value:function(e,t){t=t||{};var a=new CustomEvent(e,{detail:t});document.dispatchEvent(a)}},{key:"deleteEvent",value:function(){document.removeEventListener(eventName,callback,!1)}}]),e}(),events=new Events,Markup=function(){function e(){_classCallCheck(this,e)}return _createClass(e,[{key:"create",value:function(e){var t={tag:"div",content:"",parent:"body",className:"",id:"",callback:void 0,attrs:[]};e=e||{};for(var a in t)e.hasOwnProperty(a)||(e[a]=t[a]);var n=void 0;if("input#checkbox"===e.tag?(n=document.createElement("input"),n.type="checkbox",n.value=1):n=document.createElement(e.tag),n.innerHTML=e.content,e.className&&(n.className+=e.className),e.id&&(n.id=e.id),"form"===e.tag&&(n.action="javascript:void(0)",n.method="post"),e.attrs.length&&e.attrs.forEach(function(e){for(var t in e)n[t]=e[t]}),e.parent){var o="string"==typeof e.parent?document.querySelector(e.parent):e.parent;if(!o)return console.error("No element found"),!1;o.appendChild(n)}else document.body.appendChild(n);return n}},{key:"update",value:function(e,t){this.find(e)[0].innerHTML=t}},{key:"delete",value:function(e){this.find(e)[0].remove()}},{key:"find",value:function(e){return document.querySelectorAll(e)}}]),e}(),markup=new Markup,Utils=function(){function e(){_classCallCheck(this,e)}return _createClass(e,[{key:"getId",value:function(){for(var e="",t=0;t<6;t++)e+="abcdefghijklmnopqrstuvwxyz"[Math.floor(26*Math.random())];return e}}]),e}(),utils=new Utils,AddTodo=function(){function e(){var t=this;_classCallCheck(this,e);var a=markup.create({className:"row",parent:"#todo_add"}),n=markup.create({className:"col-lg-12",parent:a}),o=markup.create({tag:"form",attrs:[{action:"javascript:void(0)"},{method:"POST"}],parent:n}),r=markup.create({className:"input-group",parent:o}),c=markup.create({tag:"input",attrs:[{type:"text"},{placeholder:"Todo text..."}],className:"form-control",parent:r}),s=markup.create({tag:"span",className:"input-group-btn",parent:r}),i=markup.create({tag:"button",attrs:[{type:"submit"}],content:"Add",className:"btn btn-primary",parent:s});events.on(o,"submit",function(e){e.preventDefault(),t.add(o,c)}),events.subscribe("edit-todo-item",function(e){var a=e.detail;a.todo&&t.edit(o,c,i,a)})}return _createClass(e,[{key:"add",value:function(e,t){var a=data.read("todos");a?a=JSON.parse(a):(a=[],data.create("todos",JSON.stringify([]))),e.type&&"save"===e.type.value&&e.index?(a[+e.index.value].text=t.value,e.type.remove(),e.index.remove()):a.push({text:t.value,checked:!1}),data.update("todos",JSON.stringify(a)),e.reset(),events.send("get-todos-list")}},{key:"edit",value:function(e,t,a,n){if(e.type)e.type.value="save";else{markup.create({tag:"input",attrs:[{type:"hidden"},{name:"type"},{value:"save"}],className:"form-control",parent:e})}if(e.index)e.index.value=n.index;else{markup.create({tag:"input",attrs:[{type:"hidden"},{name:"index"},{value:n.index}],className:"form-control",parent:e})}a.innerHTML="Save",t.value=n.todo.text}}]),e}(),ListTodos=function(){function e(){var t=this;_classCallCheck(this,e);var a=data.read("todos");a||(a=[],data.update("todos",JSON.stringify(a))),events.subscribe("get-todos-list",function(){t.getList()})}return _createClass(e,[{key:"getList",value:function(){var e=this,t=data.read("todos");t=JSON.parse(t);var a=document.getElementById("todo_items");a.innerHTML="";var n=markup.create({tag:"tbody",parent:a});if(!t.length)return!1;t.forEach(function(a,o){var r=markup.create({tag:"tr",parent:n,className:a.checked?"success":""}),c=markup.create({tag:"td",parent:r,attrs:[{width:"30"}]}),s=markup.create({tag:"td",parent:r,content:a.text,className:a.checked?"checked":""}),i=markup.create({tag:"td",parent:r,attrs:[{width:"70"}]}),l=markup.create({tag:"span",className:"glyphicon glyphicon-"+(a.checked?"check":"unchecked"),parent:c}),u=markup.create({tag:"button",attrs:[{type:"button"}],className:"btn btn-info btn-xs",content:'<span class="glyphicon glyphicon-pencil"></span>',parent:i}),d=markup.create({tag:"button",attrs:[{type:"button"}],className:"btn btn-danger btn-xs",content:'<span class="glyphicon glyphicon-remove"></span>',parent:i});events.on(c,"click",function(n){n.preventDefault(),e.doCheck(r,l,s,t,a,o)}),events.on(s,"click",function(n){n.preventDefault(),e.doCheck(r,l,s,t,a,o)}),events.on(d,"click",function(a){e.delete(o,t)}),events.on(u,"click",function(e){events.send("edit-todo-item",{todo:a,index:o})})})}},{key:"doCheck",value:function(e,t,a,n,o,r){"glyphicon glyphicon-check"===t.className?(e.className="",t.className="glyphicon glyphicon-unchecked",a.className="",o.checked=!1):(e.className="success",t.className="glyphicon glyphicon-check",a.className="checked",o.checked=!0),n[r]=o,data.update("todos",JSON.stringify(n)),events.send("get-todos-list")}},{key:"delete",value:function(e,t){t.splice(e,1),data.update("todos",JSON.stringify(t)),events.send("get-todos-list")}}]),e}(),App=function e(){_classCallCheck(this,e);new AddTodo;(new ListTodos).getList()},app=new App;
-//# sourceMappingURL=app.js.map
+
+class Data {
+
+  create(key, data) {
+    key = key || prompt('Key:');
+    data = data || prompt('Data:');
+
+    if (typeof key === 'undefined') {
+      console.error('No key');
+      return false;
+    }
+
+    if (typeof data === 'undefined') {
+      console.error('No data');
+      return false;
+    }
+
+    localStorage.setItem(key, data);
+  }
+
+  read(key) {
+    if (typeof key === 'undefined') {
+      console.error('No data');
+      return false;
+    }
+
+    const data = localStorage.getItem(key);
+
+    if (!data) {
+      console.error('No data');
+      return false;
+    }
+
+    return data;
+  }
+
+  update(key, data) {
+    key = key || prompt('Key:');
+    data = data || prompt('Data:');
+
+    if (typeof key === 'undefined') {
+      console.error('No key');
+      return false;
+    }
+
+    if (typeof data === 'undefined') {
+      console.error('No data');
+      return false;
+    }
+
+    if (!localStorage.getItem(key)) {
+      console.error('No data found');
+      return false;
+    }
+
+    localStorage.setItem(key, data);
+  }
+
+  delete(key) {
+    key = key || prompt('Key:');
+
+    if (typeof key === 'undefined') {
+      console.error('No key');
+      return false;
+    }
+
+    if (!localStorage.getItem(key)) {
+      console.error('No data found');
+      return false;
+    }
+
+    localStorage.removeItem(key);
+  }
+}
+
+const data = new Data();
+
+class Events {
+
+  on(element, eventName, callback) {
+    element.addEventListener(eventName, callback, false);
+  }
+
+  subscribe(eventName, callback) {
+    document.addEventListener(eventName, callback, false);
+  }
+
+  send(eventName, details) {
+    details = details || {};
+
+    const event = new CustomEvent(eventName, {
+      detail: details
+    });
+
+    document.dispatchEvent(event);
+  }
+
+  deleteEvent() {
+    document.removeEventListener(eventName, callback, false);
+  }
+}
+
+const events = new Events();
+
+class Markup {
+
+  create(options) {
+    // tag
+    // content
+    // parent
+    // className
+    // id
+    // callback
+
+    const optionsDefault = {
+      tag: 'div',
+      content: '',
+      parent: 'body',
+      className: '',
+      id: '',
+      callback: undefined,
+      attrs: []
+    };
+
+    options = options || {};
+
+    for(let i in optionsDefault) {
+      if (!options.hasOwnProperty(i)) {
+        options[i] = optionsDefault[i];
+      }
+    }
+
+    let element;
+
+    if (options.tag === 'input#checkbox') {
+      element = document.createElement('input');
+      element.type = 'checkbox';
+      element.value = 1;
+    } else {
+      element = document.createElement(options.tag);
+    }
+
+    element.innerHTML = options.content;
+
+    if (options.className) {
+      element.className += options.className;
+    }
+
+    if (options.id) {
+      element.id = options.id;
+    }
+
+    if (options.tag === 'form') {
+      element.action = 'javascript:void(0)';
+      element.method = 'post';
+    }
+
+    if (options.attrs.length) {
+      options.attrs.forEach(attr => {
+        for (const name in attr) {
+          switch (name) {
+            default:
+              element[name] = attr[name];
+              break;
+          }
+        }
+      });
+    }
+
+    if (options.parent) {
+      const prnt = typeof options.parent === 'string'
+        ? document.querySelector(options.parent)
+        : options.parent;
+
+      if (!prnt) {
+        console.error('No element found');
+        return false;
+      }
+      prnt.appendChild(element);
+    } else {
+      document.body.appendChild(element);
+    }
+    return element;
+  }
+
+  update(query, content) {
+    const elements = this.find(query);
+    elements[0].innerHTML = content;
+  }
+
+  delete(query) {
+    const elements = this.find(query);
+    elements[0].remove();
+  }
+
+  find(query) {
+    const elements = document.querySelectorAll(query);
+    return elements;
+  }
+}
+
+const markup = new Markup();
+
+
+class Utils {
+
+  getId() {
+    let letters = 'abcdefghijklmnopqrstuvwxyz';
+    let id = '';
+    for (let i = 0; i < 6; i++ ) {
+      id += letters[Math.floor(Math.random() * 26)];
+    }
+    return id;
+  }
+}
+
+const utils = new Utils();
+
+class AddTodo {
+
+  constructor() {
+    const row = markup.create({
+      className: 'row',
+      parent: '#todo_add'
+    });
+
+    const cell = markup.create({
+      className: 'col-lg-12',
+      parent: row
+    });
+    
+    const form = markup.create({
+      tag: 'form',
+      attrs: [
+        { action: 'javascript:void(0)' },
+        { method: 'POST' }
+      ],
+      parent: cell
+    });
+    
+    const inputGroup = markup.create({
+      className: 'input-group',
+      parent: form
+    });
+
+    const addField = markup.create({
+      tag: 'input',
+      attrs: [
+        { type: 'text' },
+        { placeholder: 'Todo text...' }
+      ],
+      className: 'form-control',
+      parent: inputGroup
+    });
+
+    const buttonGroup = markup.create({
+      tag: 'span',
+      className: 'input-group-btn',
+      parent: inputGroup
+    });
+
+    const addButton = markup.create({
+      tag: 'button',
+      attrs: [
+        { type: 'submit' }
+      ],
+      content: 'Add',
+      className: 'btn btn-primary',
+      parent: buttonGroup
+    });
+
+    events.on(form, 'submit', event => {
+      event.preventDefault();
+      this.add(form, addField);
+    });
+
+    events.subscribe('edit-todo-item', event => {
+      var details = event.detail;
+
+      if (!details.todo) { return }
+
+      this.edit(form, addField, addButton, details);
+    });
+  }
+
+  add(form, addField) {
+    let todos = data.read('todos');
+
+    if (!todos) {
+      todos = [];
+      data.create('todos', JSON.stringify([]));
+    } else {
+      todos = JSON.parse(todos);
+    }
+
+    if (form.type && form.type.value === 'save' && form.index) {
+      todos[+form.index.value].text = addField.value;
+      form.type.remove();
+      form.index.remove();
+    } else {
+      todos.push({
+        text: addField.value,
+        checked: false
+      });
+    }
+
+    data.update('todos', JSON.stringify(todos));
+
+    form.reset();
+
+    events.send('get-todos-list');
+  }
+
+  edit(form, addField, addButton, details) {
+    if (!form.type) {
+      var typeField = markup.create({
+        tag: 'input',
+        attrs: [
+          { type: 'hidden' },
+          { name: 'type' },
+          { value: 'save' }
+        ],
+        className: 'form-control',
+        parent: form
+      });
+    } else {
+      form.type.value = 'save';
+    }
+
+    if (!form.index) {
+      var indexField = markup.create({
+        tag: 'input',
+        attrs: [
+          { type: 'hidden' },
+          { name: 'index' },
+          { value: details.index }
+        ],
+        className: 'form-control',
+        parent: form
+      });
+    } else {
+      form.index.value = details.index;
+    }
+
+    addButton.innerHTML = 'Save';
+    addField.value = details.todo.text;
+  }
+}
+
+
+class ListTodos {
+
+  constructor() {
+    let todos = data.read('todos');
+    if (!todos) {
+      todos = [];
+      data.update('todos', JSON.stringify(todos));
+    }
+
+    events.subscribe('get-todos-list', () => {
+      this.getList();
+    });
+  }
+
+  getList() {
+    const self = this;
+    let todos = data.read('todos');
+    todos = JSON.parse(todos);
+
+    const todosEl = document.getElementById('todo_items');
+    todosEl.innerHTML = '';
+
+    const todosBodyEl = markup.create({
+      tag: 'tbody',
+      parent: todosEl
+    });
+
+    if (!todos.length) {
+      return false
+    }
+
+    todos.forEach((todo, index) => {
+      const todoEl = markup.create({
+        tag: 'tr',
+        parent: todosBodyEl,
+        className: todo.checked ? 'success' : ''
+      });
+
+      const todoCellCheckboxEl = markup.create({
+        tag: 'td',
+        parent: todoEl,
+        attrs: [
+          { width: '30' }
+        ]
+      });
+
+      const todoCellTextEl = markup.create({
+        tag: 'td',
+        parent: todoEl,
+        content: todo.text,
+        className: todo.checked ? 'checked' : ''
+      });
+
+      const todoCellActionsEl = markup.create({
+        tag: 'td',
+        parent: todoEl,
+        attrs: [
+          { width: '70' }
+        ]
+      });
+
+      const todoCheckboxEl = markup.create({
+        tag: 'span',
+        className: 'glyphicon glyphicon-' + (todo.checked ? 'check' : 'unchecked'),
+        parent: todoCellCheckboxEl
+      });
+
+      const todoActionEditEl = markup.create({
+        tag: 'button',
+        attrs: [
+          { type: 'button' }
+        ],
+        className: 'btn btn-info btn-xs',
+        content: '<span class="glyphicon glyphicon-pencil"></span>',
+        parent: todoCellActionsEl
+      });
+
+      const todoActionDeleteEl = markup.create({
+        tag: 'button',
+        attrs: [
+          { type: 'button' }
+        ],
+        className: 'btn btn-danger btn-xs',
+        content: '<span class="glyphicon glyphicon-remove"></span>',
+        parent: todoCellActionsEl
+      });
+
+      events.on(todoCellCheckboxEl, 'click', event => {
+        event.preventDefault();
+        self.doCheck(todoEl, todoCheckboxEl, todoCellTextEl, todos, todo, index);
+      });
+
+      events.on(todoCellTextEl, 'click', event => {
+        event.preventDefault();
+        self.doCheck(todoEl, todoCheckboxEl, todoCellTextEl, todos, todo, index);
+      });
+
+      events.on(todoActionDeleteEl, 'click', event => {
+        self.delete(index, todos);
+      });
+
+      events.on(todoActionEditEl, 'click', event => {
+        events.send('edit-todo-item', {
+          todo: todo,
+          index: index
+        });
+      });
+    });
+  };
+
+  doCheck(todoEl, todoCheckboxEl, todoCellTextEl, todos, todo, index) {
+    const isChecked = todoCheckboxEl.className === 'glyphicon glyphicon-check'
+
+    if (isChecked) {
+      todoEl.className = '';
+      todoCheckboxEl.className = 'glyphicon glyphicon-unchecked';
+      todoCellTextEl.className = '';
+      todo.checked = false;
+    } else {
+      todoEl.className = 'success';
+      todoCheckboxEl.className = 'glyphicon glyphicon-check';
+      todoCellTextEl.className = 'checked';
+      todo.checked = true;
+    }
+
+    todos[index] = todo;
+    data.update('todos', JSON.stringify(todos));
+    events.send('get-todos-list');
+  }
+
+  delete(index, todos) {
+    todos.splice(index, 1);
+    data.update('todos', JSON.stringify(todos));
+    events.send('get-todos-list');
+  }
+}
+
+
+class App {
+  constructor() {
+    // var todos = data.read('todos');
+    const todos = [];
+
+    for (let i = 0; i < 1000; i++) {
+      todos.push({
+        text: 'Test',
+        checked: false
+      });
+    }
+
+    data.update('todos', JSON.stringify(todos));
+
+    const addTodo = new AddTodo();
+    const listTodos = new ListTodos();
+    listTodos.getList();
+    window.parent.postMessage('FRAME_LOADED','http://localhost:3000');
+  }
+}
+
+
+const app = new App();
--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,19 @@
 
 class App {
   constructor() {
+    window.parent.postMessage('FRAME_LOADED','https://jsmeasure.herokuapp.com');
+    // var todos = data.read('todos');
+    const todos = [];
+
+    for (let i = 0; i < 1000; i++) {
+      todos.push({
+        text: 'Test',
+        checked: false
+      });
+    }
+
+    data.update('todos', JSON.stringify(todos));
+
     const addTodo = new AddTodo();
     const listTodos = new ListTodos();
     listTodos.getList();
